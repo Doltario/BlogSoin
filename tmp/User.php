29fabf9bbbc53e58ce2e668d7350920902faa5cc
Doltario <felixdarraud@MacBook-Pro-de-Felix.local>
2017-03-16 13:49:14 +0100

    yolo

--- tmp/User.php
+++ tmp/User.php
@@ -51,5 +51,28 @@ class User
     {
         return $this->name;
     }
-}
 
+    /**
+     * Set password
+     *
+     * @param string $password
+     *
+     * @return User
+     */
+    public function setPassword($password)
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+
+    /**
+     * Get password
+     *
+     * @return string
+     */
+    public function getPassword()
+    {
+        return $this->password;
+    }
+}
