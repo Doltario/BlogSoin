13fd4cdb3be4371ef98ec4f7200f2ccffec7fac0
root <maxime.ferraille@gmail.com>
2017-03-16 12:05:41 +0100

    Essai

--- class/User.php
+++ class/User.php
@@ -9,38 +9,38 @@
     /** @Column(type="string", length=160, nullable=true) **/
       protected $name;
 
-    /**
-     * Get id
-     *
-     * @return integer
-     */
-    public function getId()
-    {
-        return $this->id;
-    }
+  /**
+   * Get id
+   *
+   * @return integer
+   */
+  public function getId()
+  {
+      return $this->id;
+  }
 
-    /**
-     * Set name
-     *
-     * @param string $name
-     *
-     * @return User
-     */
-    public function setName($name)
-    {
-        $this->name = $name;
+  /**
+   * Set name
+   *
+   * @param string $name
+   *
+   * @return User
+   */
+  public function setName($name)
+  {
+      $this->name = $name;
 
-        return $this;
-    }
+      return $this;
+  }
 
-    /**
-     * Get name
-     *
-     * @return string
-     */
-    public function getName()
-    {
-        return $this->name;
-    }
+  /**
+   * Get name
+   *
+   * @return string
+   */
+  public function getName()
+  {
+      return $this->name;
+  }
 }
 ?>
