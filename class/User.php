9c193d52e68f527e065491d1265266afa13fe41a
Doltario <felixdarraud@MacBook-Pro-de-Felix.local>
2017-03-16 11:18:53 +0100

    init

---
+++ class/User.php
@@ -0,0 +1,46 @@
+<?php
+  // src/User.php
+  /**
+   * @Entity @Table(name="users")
+   **/
+  class User{
+    /** @Id @Column(type="integer") @GeneratedValue **/
+      protected $id;
+    /** @Column(type="string", length=160, nullable=true) **/
+      protected $name;
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     *
+     * @return User
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+}
+?>
