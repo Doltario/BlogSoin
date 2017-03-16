1185f4919a023d74f18087e15313a480aad3693e
Doltario <felixdarraud@MacBook-Pro-de-Felix.local>
2017-03-16 14:08:15 +0100

    article/comment : champs sans relations

--- class/Comment.php
+++ class/Comment.php
@@ -6,66 +6,10 @@
   class User{
     /** @Id @Column(type="integer") @GeneratedValue **/
       protected $id;
-    /** @Column(type="string", length=160, nullable=true) **/
-      protected $name;
-    /** @Column(type="string", length=160, nullable=true) **/
-      protected $password;
+    /** @Column(type="text", length=5000, nullable=true) **/
+      protected $content;
+    /** @Column(type="datetime", length=50, nullable=true) **/
+      protected $dateTime;
 
-    /**
-     * Get id
-     *
-     * @return integer
-     */
-    public function getId()
-    {
-        return $this->id;
-    }
-
-    /**
-     * Set name
-     *
-     * @param string $name
-     *
-     * @return User
-     */
-    public function setName($name)
-    {
-        $this->name = $name;
-
-        return $this;
-    }
-
-    /**
-     * Get name
-     *
-     * @return string
-     */
-    public function getName()
-    {
-        return $this->name;
-    }
-    /**
-     * Set password
-     *
-     * @param string $password
-     *
-     * @return User
-     */
-    public function setPassword($password)
-    {
-        $this->password = $password;
-
-        return $this;
-    }
-
-    /**
-     * Get password
-     *
-     * @return string
-     */
-    public function getPassword()
-    {
-        return $this->password;
-    }
 }
 ?>
